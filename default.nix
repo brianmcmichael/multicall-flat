d28edf1391c302ac0086469672bf423df27445e4
Christopher Fredén <me@icetan.org>
2019-10-31 13:45:06 +0100

    Add ds-guard

--- default.nix
+++ default.nix
@@ -1,12 +1,16 @@
 { srcRoot ? null }:
 
 let
-  inherit (builtins) mapAttrs;
+  inherit (builtins) mapAttrs all attrNames;
+  #dappPkgs = import ~/src/maker/dapptools {};
   dappPkgs = import (fetchGit {
     url = "https://github.com/icetan/dapptools";
     ref = "nix-solpkg-flatten";
+    rev = "9ff641f442b812e8a07a04f8fc97986e03647dae";
   }) {};
   inherit (dappPkgs.callPackage ./dapp2.nix { inherit srcRoot; })
     specs packageSpecs;
-  flatSpecs = mapAttrs (_: spec: spec // { flatten = true; })  specs.this.deps;
+  flatSpecs = mapAttrs
+    (_: spec: spec // { flatten = true; })
+    specs.this.deps;
 in packageSpecs flatSpecs
