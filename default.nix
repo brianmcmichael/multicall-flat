54ca767eb58139c1f5bda84194f616c11956cc77
Christopher Fredén <me@icetan.org>
2019-11-15 07:23:52 +0100

    Update dapp2nix

--- default.nix
+++ default.nix
@@ -1,12 +1,14 @@
-{ srcRoot ? null }:
-
-let
-  inherit (builtins) mapAttrs all attrNames;
-  dappPkgs = import (fetchGit {
+{ srcRoot ? null
+, dappPkgsSrc ? fetchGit {
     url = "https://github.com/icetan/dapptools";
     ref = "nix-solpkg-flatten";
     rev = "f8d7460249ebfea35632ca3848716574e94dfb5f";
-  }) {};
+  }
+}:
+
+let
+  inherit (builtins) mapAttrs;
+  dappPkgs = import dappPkgsSrc {};
   inherit (dappPkgs.callPackage ./dapp2.nix { inherit srcRoot; })
     specs packageSpecs;
   flatSpecs = mapAttrs
--- shell.nix
+++ shell.nix
@@ -1,8 +1,8 @@
 let
   dapp2nix = import (fetchGit {
     url = "https://github.com/icetan/dapp2nix";
-    ref = "v2.1.3";
-    rev = "a82d8b06341a2fd53419831c6464006ecb8c8190";
+    ref = "v2.1.6";
+    rev = "f69bccc4aaaa2be30bfd52d005c3d7edfe86e855";
   }) {};
 in (import <nixpkgs> {}).mkShell {
   buildInputs = [ dapp2nix ];
